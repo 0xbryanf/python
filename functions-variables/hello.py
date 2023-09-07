11533c2f6e303b7b224e4e0a4a2fd054705c23f4
0xbryanf <0xbryanf@gmail.com>
2023-09-07 20:12:57 +0800

    modified functions-variables

---
+++ functions-variables/hello.py
@@ -0,0 +1,14 @@
+name = input("What's your name? ")
+"""
+Run python hello.py to log What's your name? 
+This will ask you to enter your name and save it to the 'name' variable.
+"""
+
+print("hello,")
+#Run python hello.py to log the "hello,"
+
+print(name)
+#Run python hello.py to log tha name saved on variable 'name'.
+
+print("hello, " + name)
+#Run python hello.py to log the string "hello," + name variable.
